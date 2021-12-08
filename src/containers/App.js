0341da7ff54e5537426d1906b26caf21e70269dd
the-dsw <fmod@the-dsw.com>
2021-12-08 10:51:53 +0100

    add redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-function App() {
-	const [robots, setRobots] = useState( []);
-	const [searchfield, setSearchfield] = useState( '')
-  
-	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users =>  setRobots(users))
-	}, [])
+import { setSearchField, requestRobots } from '../actions';
 
- const  onSearchChange = (event) => {
-		setSearchfield(event.target.value)
-  }
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
+function App(props) {
+	const  { searchField, onSearchChange,onRequestRobots, robots, isPending } = props
+
+	useEffect(() => {
+		onRequestRobots()
+	}, [onRequestRobots])
 
 	const filteredRobots =  robots.filter(robot => {
-		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
 	})
-	return !robots.length ? 
+	return isPending ? 
 		<h1>Loading...</h1> :
 	(
 			<div className='tc'>
@@ -37,4 +49,4 @@ function App() {
 		)
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)( App);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createLogger  } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware,logger)),
+  /*applyMiddleware(logger)*/
+   );
 
 
 ReactDOM.render(
-  <React.StrictMode>
+ <Provider store={store}>
     <App  />
-  </React.StrictMode>,
+ </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
